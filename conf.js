94e38b2bf2f7c17bdd7de4238edb9d8aba8946d8
vltdxchange <vltdxchange@gmail.com>
2020-08-15 12:11:27 +0530

    jenkinsfile

--- conf.js
+++ conf.js
@@ -1,25 +1,28 @@
 exports.config = {
-    directConnect: true,
-    
-    // The address of a running selenium server.
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-  
-    // Capabilities to be passed to the webdriver instance.
-    capabilities: {
-      browserName: 'chrome'
-      
-    },
-  
-    // Spec patterns are relative to the configuration file location passed
-    // to protractor (in this example conf.js).
-    // They may include glob patterns.
-    specs: ['../pipeline/src/specs/login*.js'],
-    onPrepare: function () {
-        browser.ignoreSynchronization = true;      
-    },
-    // Options to be passed to Jasmine-node.
-    jasmineNodeOpts: {
-      showColors: true, // Use colors in the command line report.
-    }
-  };
-  
+  directConnect: true,
+
+  // The address of a running selenium server.
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    browserName: 'chrome'
+
+  },
+
+  // Spec patterns are relative to the configuration file location passed
+  // to protractor (in this example conf.js).
+  // They may include glob patterns.
+  specs: ['../pipeline/src/specs/login*.js'],
+  onPrepare: function () {
+    browser.ignoreSynchronization = true;
+    browser.manage().deleteAllCookies();
+    browser.manage().timeouts().pageLoadTimeout(50000);
+    browser.manage().timeouts().implicitlyWait(50000);
+    browser.driver.manage().window().maximize();
+  },
+  // Options to be passed to Jasmine-node.
+  jasmineNodeOpts: {
+    showColors: true, // Use colors in the command line report.
+  }
+};
