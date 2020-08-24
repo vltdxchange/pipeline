b785766e6dddc2f19ce31de675fb85532ca999cc
vltdxchange <vltdxchange@gmail.com>
2020-08-24 19:34:29 +0530

    anil

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ exports.config = {
   directConnect: true,
 
   // The address of a running selenium server.
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+ // seleniumAddress: 'http://localhost:4444/wd/hub',
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
